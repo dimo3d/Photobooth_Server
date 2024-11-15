ac4d81828ad963c772911258e1c92d04f5691481
dimo3d <dieter.morgenroth@web.de>
2024-11-15 19:04:36 +0100

    debug off

--- flask-server/app/main.py
+++ flask-server/app/main.py
@@ -130,4 +130,4 @@ def upload_processed_image(image_id):
     
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
